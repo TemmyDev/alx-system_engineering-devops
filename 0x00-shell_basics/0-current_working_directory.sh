e371fc91fe2934f5da8f830ba908fbfe05ea4441
Temilorun Lajorin <tlajorin@gmail.com>
2022-08-31 05:26:35 -0700

    added script

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,13 +1,2 @@
-Xth=$(realpath “${BASH_SOURCE:-$0}”)
-
-echo ‘The absolute path is’ $path
-
-
-
-echo ‘-----------------------------------’
-
-
-
-DIR_PATH=$(dirname $path)
-
-echo ‘The directory path is’ $DPATH
+#!/bin/bash
+pwd
